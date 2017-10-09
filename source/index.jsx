e36b1a2acb9d3f3c82c1c259fe2870f9c1328b98
Siyang Liu <supersiioung@gmail.com>
2017-10-09 17:20:56 -0500

    render home

--- source/index.jsx
+++ source/index.jsx
@@ -6,6 +6,10 @@ import 'semantic-ui-css/semantic.min.css';
 
 // Include your new Components here
 import Home from './components/Home/Home.jsx';
+import ListView from './components/ListView/ListView.jsx';
+import GalleryView from './components/GalleryView/GalleryView.jsx';
+import DetailsView from './components/DetailsView/DetailsView.jsx';
+import RouterView from './RouterView.jsx'
 
 // Include any new stylesheets here
 // Note that components' stylesheets should NOT be included here.
